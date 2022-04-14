2e46bc3e009f763968dec9e005aa6bf07f05ffff
LJC-DB <leonardojcarmo@hotmail.com>
2022-04-14 17:21:30 -0300

    feat(RecyclingGuide): add simple RecyclerView

--- app/src/main/java/br/edu/ufabc/reciclabc/ui/recyclingguide/RecyclingGuideFragment.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/ui/recyclingguide/RecyclingGuideFragment.kt
@@ -4,39 +4,64 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import br.edu.ufabc.reciclabc.databinding.FragmentRecyclingGuideBinding
+import br.edu.ufabc.reciclabc.databinding.RecyclingGuideItemBinding
+import br.edu.ufabc.reciclabc.model.RecyclingInformation
 
 class RecyclingGuideFragment : Fragment() {
 
-    private var _binding: FragmentRecyclingGuideBinding? = null
+    private lateinit var binding: FragmentRecyclingGuideBinding
+    private val viewModel: RecyclingGuideViewModel by viewModels()
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private inner class RecyclingGuideAdapter(val recycling_info: List<RecyclingInformation>) : RecyclerView.Adapter<RecyclingGuideAdapter.RecyclingGuideHolder>() {
+
+        private inner class RecyclingGuideHolder(itemBinding: RecyclingGuideItemBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+            val title = itemBinding.recyclingGuideItemTitle
+            val content = itemBinding.recyclingGuideItemContent
+//
+//            init {
+//                itemBinding.root.setOnClickListener {
+//                    viewModel.clickedItemId.value = getItemId(bindingAdapterPosition)
+//                }
+//            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclingGuideHolder =
+            RecyclingGuideHolder(
+                RecyclingGuideItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+
+        override fun onBindViewHolder(holder: RecyclingGuideHolder, position: Int) {
+            val info = recycling_info[position]
+            holder.title.text = info.title
+            holder.content.text = info.content
+        }
+
+        override fun getItemCount(): Int = recycling_info.size
+
+        override fun getItemId(position: Int): Long = recycling_info[position].id
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(RecyclingGuideViewModel::class.java)
-
-        _binding = FragmentRecyclingGuideBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        binding = FragmentRecyclingGuideBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onStart() {
+        super.onStart()
+        binding.recyclerviewRecyclingGuide.apply {
+            adapter = RecyclingGuideAdapter(viewModel.allRecyclingInformation())
+        }
     }
 }
--- app/src/main/java/br/edu/ufabc/reciclabc/ui/recyclingguide/RecyclingGuideViewModel.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/ui/recyclingguide/RecyclingGuideViewModel.kt
@@ -1,13 +1,14 @@
 package br.edu.ufabc.reciclabc.ui.recyclingguide
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import br.edu.ufabc.reciclabc.App
 
-class RecyclingGuideViewModel : ViewModel() {
+class RecyclingGuideViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+    private val repository = (application as App).repository
+
+    fun allRecyclingInformation() = repository.getAll()
+
+    fun getRecyclingInformationById(id: Long) = repository.getById(id)
 }
