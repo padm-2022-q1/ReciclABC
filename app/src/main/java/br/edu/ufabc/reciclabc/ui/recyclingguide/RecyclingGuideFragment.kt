d1a4b675c56660e47fee5a1b66536cbb145b9c79
LJC-DB <leonardojcarmo@hotmail.com>
2022-04-14 12:15:57 -0300

    refactor: change Home to RecyclingGuide

--- app/src/main/java/br/edu/ufabc/reciclabc/ui/recyclingguide/RecyclingGuideFragment.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/ui/recyclingguide/RecyclingGuideFragment.kt
@@ -1,4 +1,4 @@
-package br.edu.ufabc.reciclabc.ui.home
+package br.edu.ufabc.reciclabc.ui.recyclingguide
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import br.edu.ufabc.reciclabc.databinding.FragmentHomeBinding
+import br.edu.ufabc.reciclabc.databinding.FragmentRecyclingGuideBinding
 
-class HomeFragment : Fragment() {
+class RecyclingGuideFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentRecyclingGuideBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(RecyclingGuideViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentRecyclingGuideBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
--- app/src/main/java/br/edu/ufabc/reciclabc/ui/recyclingguide/RecyclingGuideViewModel.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/ui/recyclingguide/RecyclingGuideViewModel.kt
@@ -1,10 +1,10 @@
-package br.edu.ufabc.reciclabc.ui.home
+package br.edu.ufabc.reciclabc.ui.recyclingguide
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class RecyclingGuideViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
