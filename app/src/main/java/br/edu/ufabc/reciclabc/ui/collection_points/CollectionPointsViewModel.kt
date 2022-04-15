1ee448d367202e5af7940096d43929039aa19b70
Leandro Trovões <leandrotrovoes@hotmail.com>
2022-04-14 21:02:10 -0300

    feat: rename dashboard to collection points

--- app/src/main/java/br/edu/ufabc/reciclabc/ui/collection_points/CollectionPointsViewModel.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/ui/collection_points/CollectionPointsViewModel.kt
@@ -1,13 +1,13 @@
-package br.edu.ufabc.reciclabc.ui.dashboard
+package br.edu.ufabc.reciclabc.ui.collection_points
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class CollectionPointsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Collection Points Fragment"
     }
     val text: LiveData<String> = _text
 }
