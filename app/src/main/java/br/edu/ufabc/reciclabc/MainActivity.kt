d0af34e5cd63a35ec64e3ac21f999517cdb29eb3
fernandokenjii <kenjifernandoo@gmail.com>
2022-04-17 04:17:45 -0300

    fix: hide bottom navigation when creating notification

--- app/src/main/java/br/edu/ufabc/reciclabc/MainActivity.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.edu.ufabc.reciclabc
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -31,6 +32,15 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_notifications
             )
         )
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.create_notification_fragment -> navView.visibility = View.GONE
+                R.id.create_address_notification_fragment -> navView.visibility = View.GONE
+                else -> navView.visibility = View.VISIBLE
+            }
+        }
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
