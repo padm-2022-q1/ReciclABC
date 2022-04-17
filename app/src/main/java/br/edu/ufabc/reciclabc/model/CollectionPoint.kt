19524cb84a205f7892e30cb4ba9f5d177c31e4c1
Leandro Trovões <leandrotrovoes@hotmail.com>
2022-04-17 17:36:14 -0300

    feat: add collection point details card (#9)
    * feat: add collection point details card
    
    * feat: focus marker and show title when selected
    
    * refactor: remove unnecessary constraint
    
    * refactor: selected marker observer logic

---
+++ app/src/main/java/br/edu/ufabc/reciclabc/model/CollectionPoint.kt
@@ -0,0 +1,9 @@
+package br.edu.ufabc.reciclabc.model
+
+data class CollectionPoint(
+    val id: Int,
+    val lat: Float,
+    val lng: Float,
+    val name: String,
+    val address: String,
+)
--- app/src/main/java/br/edu/ufabc/reciclabc/ui/collectionpoints/CollectionPointsFragment.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/ui/collectionpoints/CollectionPointsFragment.kt
@@ -1,6 +1,9 @@
 package br.edu.ufabc.reciclabc.ui.collectionpoints
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +11,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import br.edu.ufabc.reciclabc.R
 import br.edu.ufabc.reciclabc.databinding.FragmentCollectionPointsBinding
+import br.edu.ufabc.reciclabc.model.CollectionPoint
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.material.snackbar.Snackbar
 
 class CollectionPointsFragment : Fragment() {
 
@@ -27,7 +32,61 @@ class CollectionPointsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment =
-            childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment?
+            childFragmentManager.findFragmentById(R.id.collection_points_map) as SupportMapFragment?
         mapFragment?.getMapAsync(viewModel.handleMapReady)
+
+        binding.collectionPointsDetailsCard.apply {
+            // to stop both click and drag events from propagating to the map underneath
+            root.isClickable = true
+
+            cpDetailsMapButton.setOnClickListener { handleCardMapButtonClick() }
+        }
+
+        viewModel.selectedMarker.observe(viewLifecycleOwner) { handleSelectedMarkerChange(it) }
+    }
+
+    private fun handleSelectedMarkerChange(collectionPointId: Int?) {
+        if (collectionPointId == null) {
+            hideDetailsCard()
+            return
+        }
+
+        viewModel.getCollectionPointById(collectionPointId)?.let { cp -> showDetailsCard(cp) }
+            ?: run {
+                viewModel.selectedMarker.value = null
+                Log.e(
+                    "CollectionPoints",
+                    "Could not find collection point by id $collectionPointId"
+                )
+                view?.let { view ->
+                    Snackbar.make(
+                        view,
+                        "Could not load collection point details",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
+    private fun showDetailsCard(collectionPoint: CollectionPoint) {
+        binding.collectionPointsDetailsCard.apply {
+            cpDetailsTitle.text = collectionPoint.name
+            cpDetailsAddress.text = collectionPoint.address
+            root.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideDetailsCard() {
+        binding.collectionPointsDetailsCard.root.visibility = View.GONE
+    }
+
+    private fun handleCardMapButtonClick() {
+        viewModel.selectedMarker.value?.let {
+            viewModel.getCollectionPointById(it)?.let { cp ->
+                val mapIntentUri = Uri.parse("geo:${cp.lat},${cp.lng}?q=${Uri.encode(cp.name)}")
+                val mapIntent = Intent(Intent.ACTION_VIEW, mapIntentUri)
+                startActivity(mapIntent)
+            }
+        }
     }
 }
