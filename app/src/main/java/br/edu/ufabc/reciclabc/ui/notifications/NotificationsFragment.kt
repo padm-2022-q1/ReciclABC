376f6a76644feffd8ff6480cf2ad424395202afc
fernandokenjii <kenjifernandoo@gmail.com>
2022-04-17 04:18:33 -0300

    feat: add safe args

--- app/src/main/java/br/edu/ufabc/reciclabc/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/ui/notifications/NotificationsFragment.kt
@@ -6,8 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import br.edu.ufabc.reciclabc.databinding.FragmentNotificationsBinding
+import br.edu.ufabc.reciclabc.model.AddressNotification
 
 class NotificationsFragment : Fragment() {
     private lateinit var binding: FragmentNotificationsBinding
@@ -28,11 +29,28 @@ class NotificationsFragment : Fragment() {
         super.onStart()
 
         binding.rvNotifications.apply {
+            val onEditAddressNotificationClicked = { addressNotification: AddressNotification ->
+                val action = NotificationsFragmentDirections.createAddressNotificationAction(
+                    addressNotification
+                )
+                this.findNavController().navigate(action)
+            }
+
+            val onDeleteAddressNotificationClicked = { addressNotificationId: Long ->
+                // TODO: Delete address notification
+//                viewModel.delete(addressNotificationId)
+            }
+
             adapter = AddressNotificationAdapter(
-                viewModel.allAddressNotification()
+                viewModel.allAddressNotification(),
+                onEditAddressNotificationClicked,
+                onDeleteAddressNotificationClicked,
             )
         }
 
+        binding.floatingActionButton.setOnClickListener {
+            val action = NotificationsFragmentDirections.createAddressNotificationAction()
+            it.findNavController().navigate(action)
+        }
     }
-
 }
