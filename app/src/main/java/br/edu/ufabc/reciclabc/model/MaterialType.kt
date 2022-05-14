e17389de62610cbafb0413305d9341284bd86639
fernandokenjii <kenjifernandoo@gmail.com>
2022-05-14 01:14:33 -0300

    feat: add material type to collection point

---
+++ app/src/main/java/br/edu/ufabc/reciclabc/model/MaterialType.kt
@@ -0,0 +1,12 @@
+package br.edu.ufabc.reciclabc.model
+
+enum class MaterialType {
+    BATTERIES,
+    CONSTRUCTION_WASTE,
+    ELECTRONICS,
+    GLASS,
+    COOKING_OIL,
+    METAL,
+    PAPER,
+    PLASTIC,
+}
--- app/src/main/java/br/edu/ufabc/reciclabc/model/repository/CollectionPointsRepository.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/model/repository/CollectionPointsRepository.kt
@@ -1,6 +1,7 @@
 package br.edu.ufabc.reciclabc.model.repository
 
 import br.edu.ufabc.reciclabc.model.CollectionPoint
+import br.edu.ufabc.reciclabc.model.MaterialType
 
 class CollectionPointsRepository {
     private val collectionPoints: List<CollectionPoint> = listOf(
@@ -9,14 +10,16 @@ class CollectionPointsRepository {
             -23.644837f,
             -46.528047f,
             "UFABC SA",
-            "Av. dos Estados, 5001 - Santo André - SP"
+            "Av. dos Estados, 5001 - Santo André - SP",
+            setOf(MaterialType.BATTERIES, MaterialType.PLASTIC),
         ),
         CollectionPoint(
             2,
             -23.678312f,
             -46.563312f,
             "UFABC SBC",
-            "Alameda da Universidade, s/n - São Bernardo do Campo - SP"
+            "Alameda da Universidade, s/n - São Bernardo do Campo - SP",
+            setOf(MaterialType.GLASS, MaterialType.METAL),
         ),
     )
 
