247e8ec6cdeaff3e882ce7cb620ecf78aa7d7c0e
André Omine <70779857+AOmine-27@users.noreply.github.com>
2022-05-17 18:44:54 -0300

    Feature/guide collection points data (#31)
    * add recycling guide data
    
    * add collection points data json
    
    * refactor: collection points repository to read json
    
    * recycling guide card layout changes
    
    * recycling guide text typos
    
    * remove old comments

--- app/src/main/java/br/edu/ufabc/reciclabc/App.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/App.kt
@@ -2,6 +2,7 @@ package br.edu.ufabc.reciclabc
 
 import android.app.Application
 import br.edu.ufabc.reciclabc.model.repository.AddressNotificationRepository
+import br.edu.ufabc.reciclabc.model.repository.CollectionPointsRepository
 import br.edu.ufabc.reciclabc.model.repository.RecyclingInfoRepository
 import com.google.android.gms.maps.MapsInitializer
 
@@ -12,10 +13,12 @@ class App : Application() {
      */
     val recyclingInfoRepository = RecyclingInfoRepository()
     val addressNotificationRepository = AddressNotificationRepository()
+    val collectionPointsRepository = CollectionPointsRepository()
 
     companion object {
         private var recyclingGuideFile = "recycling_info.json"
         private var addressNotificationFile = "notifications.json"
+        private var collectionPointsFile = "collection_points.json"
     }
 
     override fun onCreate() {
@@ -30,6 +33,10 @@ class App : Application() {
 
         resources.assets.open(addressNotificationFile).use {
             addressNotificationRepository.loadData(it)
+        }
+
+        resources.assets.open(collectionPointsFile).use {
+            collectionPointsRepository.loadData(it)
         }
     }
 }
--- app/src/main/java/br/edu/ufabc/reciclabc/model/repository/CollectionPointsRepository.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/model/repository/CollectionPointsRepository.kt
@@ -2,27 +2,41 @@ package br.edu.ufabc.reciclabc.model.repository
 
 import br.edu.ufabc.reciclabc.model.CollectionPoint
 import br.edu.ufabc.reciclabc.model.MaterialType
+import com.beust.klaxon.Klaxon
+import java.io.InputStream
 
 class CollectionPointsRepository {
-    private val collectionPoints: List<CollectionPoint> = listOf(
-        CollectionPoint(
-            1,
-            -23.644837f,
-            -46.528047f,
-            "UFABC SA",
-            "Av. dos Estados, 5001 - Santo André - SP",
-            MaterialType.values().toSet(),
-        ),
-        CollectionPoint(
-            2,
-            -23.678312f,
-            -46.563312f,
-            "UFABC SBC",
-            "Alameda da Universidade, s/n - São Bernardo do Campo - SP",
-            setOf(MaterialType.GLASS, MaterialType.METAL),
-        ),
-    )
+    private lateinit var collectionPoints: List<CollectionPoint>
 
-    fun getAll(): List<CollectionPoint> = collectionPoints
-    fun getById(id: Int): CollectionPoint? = collectionPoints.find { it.id == id }
+    private data class CollectionPointJson(
+        val id: Int,
+        val lat: String,
+        val lng: String,
+        val name: String,
+        val address: String,
+        val materials: String
+    ) {
+        fun toCollectionPoints() = CollectionPoint(
+            id,
+            lat.toFloat(),
+            lng.toFloat(),
+            name,
+            address,
+            materials.split(",").map { MaterialType.valueOf(it) }.toSet()
+        )
+    }
+
+    fun loadData(inputStream: InputStream) {
+        collectionPoints = Klaxon().parseArray<CollectionPointJson>(inputStream)?.map {
+            it.toCollectionPoints()
+        }?: emptyList()
+    }
+
+    fun getAll(): List<CollectionPoint> =
+        if (this::collectionPoints.isInitialized) collectionPoints
+        else throw UninitializedPropertyAccessException("Load data first")
+
+    fun getById(id: Int): CollectionPoint? =
+        if (this::collectionPoints.isInitialized) collectionPoints.find { it.id == id }
+        else throw UninitializedPropertyAccessException("Load data first")
 }
