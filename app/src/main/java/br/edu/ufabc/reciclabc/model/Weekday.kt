7c3915b8a439725c0ccde35967b906d778cf8573
Andre <dre.omine@gmail.com>
2022-05-21 12:57:04 -0300

    refactor: weekday func to numeric

--- app/src/main/java/br/edu/ufabc/reciclabc/model/Weekday.kt
+++ app/src/main/java/br/edu/ufabc/reciclabc/model/Weekday.kt
@@ -1,3 +1,3 @@
 package br.edu.ufabc.reciclabc.model
 
-enum class Weekday { SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY }
+enum class Weekday { SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY; fun toNumeric(): Int { return this.ordinal + 1 } }
